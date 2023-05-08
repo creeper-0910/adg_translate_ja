51925815dc672643bd9f836ea916e9333fbc2c54
creeper-0910 <56744841+creeper-0910@users.noreply.github.com>
2023-05-08 11:36:40 +0900

    Crowdinへの対応準備

---
+++ main.py
@@ -0,0 +1,32 @@
+import xml.etree.ElementTree as ET
+from xml.dom import minidom
+
+# XMLファイルを解析してroot要素を取得する
+tree = ET.parse('i18n/sources.xml')
+root = tree.getroot()
+
+# plurals要素を取得する
+plurals_elements = root.findall("./plurals")
+
+# strings要素を取得する
+strings_elements = root.findall("./string")
+
+# plurals.xmlファイルに書き出す
+plurals_root = ET.Element('resources')
+for plurals_element in plurals_elements:
+    plurals_root.append(plurals_element)
+plurals_string = ET.tostring(plurals_root, encoding='utf-8', xml_declaration=True)
+plurals_string = minidom.parseString(plurals_string).toprettyxml(indent='    ')
+plurals_string = '\n'.join(line for line in plurals_string.split('\n') if line.strip())
+with open('plurals.xml', 'w', encoding='utf-8') as f:
+    f.write(plurals_string)
+
+# strings.xmlファイルに書き出す
+strings_root = ET.Element('resources')
+for string_element in strings_elements:
+    strings_root.append(string_element)
+strings_string = ET.tostring(strings_root, encoding='utf-8', xml_declaration=True)
+strings_string = minidom.parseString(strings_string).toprettyxml(indent='    ')
+strings_string = '\n'.join(line for line in strings_string.split('\n') if line.strip())
+with open('strings.xml', 'w', encoding='utf-8') as f:
+    f.write(strings_string)
